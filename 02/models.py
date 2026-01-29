43c7e029d4389d48c623633e900f90ebfeb31ffd
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-29 08:33:56 +0100

    add Author and Book models with one-to-many relationship

--- 02/models.py
+++ 02/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
+
 class Author(Base):
     __tablename__ = "authors"
 
@@ -11,6 +12,7 @@ class Author(Base):
 
     books_var = relationship("Book", back_populates="author_var") # Dziwna nazwa zmiennej, ale przynajmniej nie miesza mi się z nazwą tabeli...
 
+
 class Book(Base):
     __tablename__ = "books"
 
@@ -18,4 +20,4 @@ class Book(Base):
     title = Column(String, nullable=False)
     author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
 
-    author_var = relationship("Author", back_populates="books_var")# to samo co wyżej ;) 
+    author_var = relationship("Author", back_populates="books_var") #to samo co wyżej ;) 
