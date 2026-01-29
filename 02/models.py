5b6fa24172d94f4f217b856af68f6eae71271d0f
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-29 07:49:46 +0100

    seed authors and books and add ORM queries

---
+++ 02/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    books_var = relationship("Book", back_populates="author_var") # Dziwna nazwa zmiennej, ale przynajmniej nie miesza mi się z nazwą tabeli...
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
+
+    author_var = relationship("Author", back_populates="books_var")# to samo co wyżej ;) 
