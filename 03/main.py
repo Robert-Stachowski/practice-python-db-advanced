b7601ea2c550ca3c205cd4139dc0262f3b703943
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-31 21:24:02 +0100

    Seed Person and Skill data and query relationships using SQLAlchemy ORM

--- 03/main.py
+++ 03/main.py
@@ -1,9 +1,10 @@
 from db import Session, engine
-from models import Person, Skill, Base
+from models import Person, Skill,person_skill, Base
 from sqlalchemy.exc import SQLAlchemyError
 
 
 def clear_table(session):
+    session.query(person_skill)
     session.query(Skill).delete()
     session.query(Person).delete()
     session.commit()
@@ -15,7 +16,6 @@ def main():
     with Session() as session:
         clear_table(session)
         try:
-            # --- osoby ---
             persons = [
                 Person(name="Xarion"),
                 Person(name="Lunara"),
@@ -23,8 +23,7 @@ def main():
                 Person(name="Zyra"),
                 Person(name="Olek"),
             ]
-
-            # --- umiejętności ---
+            
             skills = [
                 Skill(name="Python"),
                 Skill(name="SQL"),
@@ -38,20 +37,65 @@ def main():
                 Skill(name="Refactoring"),
             ]
 
-            session.add_all(persons+skills)
-            session.flush()
-            """
-            session.flush():
-            - wysyła INSERT/UPDATE do DB
-            - nadaje ID
-            - NIE zamyka transakcji
-            - rollback cofnie wszystko
-            """
+            session.add_all(persons+skills)            
+            session.commit()
+            print("\n Załadowano dane poprawnie \n")
+
+            #Przypisz osobom wybrane umiejętności.
+            persons = session.query(Person).all()
+            skills = session.query(Skill).all()
+
+            person_by_name = {p.name: p for p in persons}
+            skill_by_name = {s.name: s for s in skills}
+
+            assignments = {
+                "Xarion": ["Python", "SQL", "Docker"],
+                "Lunara": ["Django", "Testing"],
+                "Milo":   ["Git", "Linux"],
+                "Zyra":   ["FastAPI", "Debugging", "Refactoring"],
+                "Olek":   ["FastAPI", "Debugging", "Refactoring","Django", "Testing"]
+            }
+        
+            for person_name, skill_name in assignments.items():
+                person = person_by_name[person_name]
+                person.skills = [skill_by_name[name] for name in skill_name]
+
+            session.commit()
+            print("\n Przypisano umiejętności \n")
+
             
 
 
+            
+            
+            #Dla jednej osoby wypisz listę jej umiejętności.
+            person_name = "Olek"
+            person = session.query(Person).filter(Person.name==person_name).one()
 
-            session.commit()
+            print(f"Osoba: '{person_name}', posiada umiejętności: ")
+
+            for skills in person.skills:
+                if not person.skills:
+                    print("Brak umiejętności")
+                print(skills.name)
+
+
+
+
+
+            print("\n")
+            #Dla jednej umiejętności wypisz listę osób, które ją posiadają.
+            skill_name = "Refactoring"
+            skill = (session.query(Skill).filter(Skill.name==skill_name).one())
+
+            print(f"Skilla: '{skill.name}' posiada: ")
+
+            for person in skill.persons:
+                if not skill.persons:
+                    print(f"Nikt nie ma skilla '{skill.name}' ")
+                print(person.name)
+
+
 
 
         except SQLAlchemyError as e:
